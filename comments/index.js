666d2df7c1ba6c3d915d09e2b61b52d08d89c0c3
ttyt0416 <ttyt0416@gmail.com>
2021-02-08 13:49:25 +0900

    added express for comments, posts and react components at client

--- comments/index.js
+++ comments/index.js
@@ -1,29 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
-    res.send(commentsByPostId[req.params.id] || []);
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
 app.post('/posts/:id/comments', (req, res) => {
-    const commentId = randomBytes(4).toString('hex');
-    const { content } = req.body;
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
 
-    const comments = commentsByPostId[req.params.id] || [];
+  const comments = commentsByPostId[req.params.id] || [];
 
-    comments.push({ id: commentId, content});
+  comments.push({ id: commentId, content });
 
-    commentsByPostId[req.params.id] = comments;
+  commentsByPostId[req.params.id] = comments;
 
-    res.statys(201).send(comments);
+  res.status(201).send(comments);
 });
 
 app.listen(4001, () => {
-    console.log('Listening on 4001');
-})
+  console.log('Listening on 4001');
+});
